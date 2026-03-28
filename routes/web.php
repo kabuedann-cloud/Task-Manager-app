717a3dfde9abfe46bb32e705d679a663761879c6
kabuedann-cloud <kabuedann@gmail.com>
2026-03-28 23:48:25 +0300

    Fix: Force HTTPS and Trust Proxies for production

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ use Inertia\Inertia;
 use App\Http\Controllers\TaskController;
 
 Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
-Route::get('/report', [TaskController::class, 'report'])->name('tasks.report');
+Route::get('/tasks/report', [TaskController::class, 'report'])->name('tasks.report');
+Route::patch('/tasks/{task}/status', [TaskController::class, 'updateStatus'])->name('tasks.update-status');
 Route::resource('tasks', TaskController::class)->except(['index']);
-Route::patch('tasks/{task}/status', [TaskController::class, 'updateStatus'])->name('tasks.update-status');
